a7f007ad34dadec20631611cf51045a64a8f8f84
Tobias Roy <tobiasroy94@gmail.com>
2022-08-01 13:33:57 +0200

    This

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("This is the empty program template in vs-code!");
+﻿Console.WriteLine("Pick your option:");
+Console.WriteLine("1: Addition");
+Console.WriteLine("2. Subtraction");
+Console.WriteLine("3. Divide");
+Console.WriteLine("4. Multiply");
+
+string keyInput = Console.ReadKey();
+int input = int.Parse(keyInput);
+
+switch (input) {
+    case 1:
+        adittion();
+}
+
+void adittion() {
+    Console.WriteLine("This is addition, insert your first value and hit enter.");
+    int inputOne = int.Parse(Console.ReadLine());
+    Console.WriteLine("Insert your second value and hit enter.");
+    int inputTwo = int.Parse(Console.ReadLine());
+    int result = inputOne + inputTwo;
+    Console.WriteLine("This is your result:" + result);
+    Console.ReadKey();
+}
